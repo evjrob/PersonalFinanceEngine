f035b364404323118fe07597a11f4e5fc54bc047
Everett Robinson <evjrob@gmail.com>
2016-04-02 16:17:36 -0600

    Created and organized the files for unit testing when the canada taxModel is enabled.

---
+++ test/spec/locales/canada.spec.js
@@ -0,0 +1,17 @@
+describe("Canada tax model and calculations", function() {
+
+  describe("asset, investment, and debt constructors", function() {
+
+
+  });
+
+  describe("calculateTaxTable", function() {
+
+
+  });
+
+  describe("getTaxRate", function() {
+
+
+  });
+});
